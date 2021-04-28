78372be6adfae0575002c052f40259cf8f32818e
kmer102 <jkoni646@gmail.com>
2021-04-28 09:46:52 -0500

    Page update

--- successful_new_ad.php
+++ successful_new_ad.php
@@ -1,20 +1,17 @@
 <?php
-echo "Successful!";
+echo "";
 ?>
 
 <html>
-<button><a href="homepage.php">Back to homepage</a></button>
-</html>
-
-<html>
     <head>
-        <title> New Adversitement has been successfully added</title>
+        <title> </title>
         <meta charset="UTF-8">
         <link href="./DevProject/resources/css/successful_new_ad.css" type="text/css" rel="stylesheet">
     </head>
-    <body>
-        <div class="container-wrapper">
-            <button class="button-wrapper"><a href="homepage.php">Back to homepage</a></button>
+    <body class="container-wrapper">
+        <div class="container">
+            <h3>New Adversitement has been successfully added</h3>
+            <button class="button-wrapper"><a href="homepage.php">Homepage</a></button>
         </div> 
     </body>
 </html>
