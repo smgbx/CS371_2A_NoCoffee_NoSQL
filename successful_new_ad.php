32af6c4c4529ab65638588d0259f3b2e825efb57
kmer102 <jkoni646@gmail.com>
2021-04-28 09:15:11 -0500

    Added code to new successfully created ad

--- successful_new_ad.php
+++ successful_new_ad.php
@@ -2,4 +2,19 @@
 echo "Successful!";
 ?>
 
-<html><button><a href="homepage.php">Back to homepage</a></button></html>
+<html>
+<button><a href="homepage.php">Back to homepage</a></button>
+</html>
+
+<html>
+    <head>
+        <title> New Adversitement has been successfully added</title>
+        <meta charset="UTF-8">
+        <link href="./DevProject/resources/css/successful_new_ad.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-wrapper">
+            <button class="button-wrapper"><a href="homepage.php">Back to homepage</a></button>
+        </div> 
+    </body>
+</html>
